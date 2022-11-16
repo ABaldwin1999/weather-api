2c565c58c15ace4e8e69f09c939417604db62b45
Amelia Baldwin <amelia01000010@gmail.com>
2022-11-16 16:19:36 +0000

    styling

--- src/Components/WeatherInfo/WeatherInfo.jsx
+++ src/Components/WeatherInfo/WeatherInfo.jsx
@@ -6,8 +6,8 @@ const WeatherInfo = ({text,img,temp}) => {
       <h2 className='forecast__temp'>{temp + "\u00B0" + "C"}</h2>
       <div className='forecast__break'></div>
       <div className='forecast__weather'>
-        <img src={img} alt={text} /> 
-        <p>{text}</p>
+        <img  className='forecast__weather' src={img} alt={text} /> 
+        <p  className='forecast__weather'>{text}</p>
       </div>
       
     </div>
